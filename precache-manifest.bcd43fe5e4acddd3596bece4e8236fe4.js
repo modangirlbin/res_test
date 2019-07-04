82b13253f2693eea565ee3983be13544897f9a07
modangirl <modangirl@gmail.com>
2019-07-04 21:50:31 +0900

    Updates

---
+++ precache-manifest.bcd43fe5e4acddd3596bece4e8236fe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c87b3e51e37d1238419",
+    "url": "/res_test/static/js/runtime~main.2c87b3e5.js"
+  },
+  {
+    "revision": "7d602841472a72e9e6ca",
+    "url": "/res_test/static/js/main.7d602841.chunk.js"
+  },
+  {
+    "revision": "f0c3ce5e2062243e8855",
+    "url": "/res_test/static/js/1.f0c3ce5e.chunk.js"
+  },
+  {
+    "revision": "7d602841472a72e9e6ca",
+    "url": "/res_test/static/css/main.8dee9a6d.chunk.css"
+  },
+  {
+    "revision": "0288d497e17bed33468e709cabcaede5",
+    "url": "/res_test/index.html"
+  }
+];
