6ef6b881863a5eaf9dc0543a3949b71588c929b8
modangirl <modangirl@gmail.com>
2019-07-06 00:44:23 +0900

    이미지 사이즈 및 타이틀 말줄임 수정

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,22 @@ import { Link } from 'react-router-dom';
 
 class Card extends Component {
   render() {
-    const imgUrl = this.props.imgUrl ? this.props.imgUrl : 'http://kaverisias.com/wp-content/uploads/2018/01/catalog-default-img.gif'
+    const imgUrl = this.props.imgUrl ? this.props.imgUrl : 'http://inarainbow.dothome.co.kr/sat_study/catalog-default-img.gif'
     
     let styles = {
-      height: '500px',
-      backgroundColor: 'yellow',
-    };
-
+      thum : {
+        backgroundColor: 'yellow'
+        // height: '500px',
+      },
+      tit : {
+        display: 'block',
+        display: '-webkit-box',
+        "-webkit-box-orient": 'vertical',
+        "-webkit-line-clamp": '1',
+        "text-overflow": 'ellipsis',
+        overflow: 'hidden'
+      }
+    }
 
     return (
       <li className="col">
@@ -17,11 +26,11 @@ class Card extends Component {
         <div className="card">
           <div className="content-img">
             <Link to={`/detail/${this.props.resId}`}>
-            <img src={imgUrl} alt="" style={styles}/>
+            <img src={imgUrl} alt="" style={styles.thum}/>
             </Link>
           </div>
           <div className="content-txt">
-            <h3 className="tit"><Link to={`/detail/${this.props.resId}`}>{this.props.title ? this.props.title : 'noTitle'}</Link></h3>
+            <h3 className="tit" style={styles.tit}><Link to={`/detail/${this.props.resId}`}>{this.props.title ? this.props.title : 'noTitle'}</Link></h3>
             <p className="desc">{this.props.addr ? this.props.addr : '--'}</p>
           </div>
         </div>
