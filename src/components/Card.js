2416f9d623c189ad8912134d986b070bc542035c
modangirl <modangirl@gmail.com>
2019-08-07 08:07:25 +0900

    이미지 경로 수정

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 class Card extends Component {
   render() {
-    const imgUrl = this.props.imgUrl ? this.props.imgUrl : 'http://inarainbow.dothome.co.kr/sat_study/catalog-default-img.gif'
+    const imgUrl = this.props.imgUrl ? this.props.imgUrl : '../assets/images/catalog-default-img.gif'
     
     let styles = {
       thum : {
